bd4865d1a5f2f13e5a6071384de2db7e67af385b
alumno <alumno@2A5PC25>
2015-10-16 10:16:59 +0200

    Merge origin/master
    Conflicts:
    	web/META-INF/context.xml

--- src/java/com/fpmislata/banco_api/presentation/controladores/EntidadBancariaController.java
+++ src/java/com/fpmislata/banco_api/presentation/controladores/EntidadBancariaController.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -23,17 +25,33 @@ public class EntidadBancariaController {
     @Autowired
     private JsonTransformer jsonTransformer;
 
-    @RequestMapping(value = {"/findall"})
-    public void findAll(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws IOException {
-        List<EntidadBancaria> entidadesBancarias = entidadBancariaService.findAll();
-        String jsonEntidadesBancarias = jsonTransformer.toJson(entidadesBancarias);
-        httpServletResponse.getWriter().println(jsonEntidadesBancarias);
+    @RequestMapping(value = "/entidadbancaria", method = RequestMethod.GET, produces = "application/json")
+    public void findAll(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        try {
+            String jsonEntidadesBancarias = jsonTransformer.toJson(entidadBancariaService.findAll());
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            httpServletResponse.getWriter().println(jsonEntidadesBancarias);
+        } catch (Exception ex) {
+            ex.printStackTrace(httpServletResponse.getWriter());
+        }
     }
 
-    @RequestMapping(value = {"/get"})
-    public void get(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws IOException {
-        EntidadBancaria entidadBancaria = entidadBancariaService.get(Integer.parseInt(httpRequest.getParameter("idEntidadBancaria")));
-        String jsonEntidadBancaria = jsonTransformer.toJson(entidadBancaria);
-        httpServletResponse.getWriter().println(jsonEntidadBancaria);
+    @RequestMapping(value = "/entidadbancaria/{idEntidadBancaria}", method = RequestMethod.GET, produces = "application/json")
+    public void get(@PathVariable("idEntidadBancaria") int idEntidadBancaria, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        try {
+            EntidadBancaria entidadBancaria = entidadBancariaService.get(idEntidadBancaria);
+
+            if (entidadBancaria == null) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                String jsonEntidadBancaria = jsonTransformer.toJson(entidadBancaria);
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                httpServletResponse.setContentType("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonEntidadBancaria);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(httpServletResponse.getWriter());
+        }
     }
 }
